b53948633ac6f54d1f9951899c81bb31222a0f5c
Your Name <you@example.com>
2021-12-14 00:34:26 -0300

    Initial Commit

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,31 @@
+function contar() {
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.getElementById('txtf')
+    var passo = window.document.getElementById('txtp')
+    var res = window.document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for(var c = i; c <= f; c+= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
